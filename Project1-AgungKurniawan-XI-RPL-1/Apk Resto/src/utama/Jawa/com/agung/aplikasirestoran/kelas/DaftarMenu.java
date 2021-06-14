a6dd8f203ba815908438e8797ab3ed83ffb76ca1
agungkrnwn1 <85723600+agungkrnwn1@users.noreply.github.com>
2021-06-14 19:44:56 +0700

    Update DaftarMenu.java

--- Project1-AgungKurniawan-XI-RPL-1/Apk Resto/src/utama/Jawa/com/agung/aplikasirestoran/kelas/DaftarMenu.java
+++ Project1-AgungKurniawan-XI-RPL-1/Apk Resto/src/utama/Jawa/com/agung/aplikasirestoran/kelas/DaftarMenu.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.agung.aplikasicar.classes;
+package com.agung.aplikasicar racing.classes;
 
 /**
  *
